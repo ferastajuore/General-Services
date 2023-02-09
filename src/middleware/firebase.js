48fa0ae09527cd5655a3f0db13def655e7782f8c
Feras Tajuore <42550839+ferastajuore@users.noreply.github.com>
2023-02-09 20:13:19 +0200

    Change in firebase

--- src/middleware/firebase.js
+++ src/middleware/firebase.js
@@ -4,13 +4,20 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyC9impDYKs4JX9MRftUS_RdgXd6VHGAIBc',
-	authDomain: 'project-dmi.firebaseapp.com',
-	projectId: 'project-dmi',
-	storageBucket: 'project-dmi.appspot.com',
-	messagingSenderId: '499794031439',
-	appId: '1:499794031439:web:712792dc7be6964a3bcb3a',
-	measurementId: 'G-SVKF2PTXLG',
+	// apiKey: 'AIzaSyC9impDYKs4JX9MRftUS_RdgXd6VHGAIBc',
+	// authDomain: 'project-dmi.firebaseapp.com',
+	// projectId: 'project-dmi',
+	// storageBucket: 'project-dmi.appspot.com',
+	// messagingSenderId: '499794031439',
+	// appId: '1:499794031439:web:712792dc7be6964a3bcb3a',
+	// measurementId: 'G-SVKF2PTXLG',
+	apiKey: process.env.NEXT_PUBLIC_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
